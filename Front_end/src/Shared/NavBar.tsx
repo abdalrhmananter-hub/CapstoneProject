6dfbc7584282c6e24cda0430bd4ea31c2d2d9fd2
“Ebram_Samuel” <ebramsam2001@gmail.com>
2026-05-11 18:39:54 +0300

    make the first step with navbar

--- Front_end/src/Shared/NavBar.tsx
+++ Front_end/src/Shared/NavBar.tsx
@@ -1,9 +1,20 @@
+import logo from "../assets/logo.png";
 
-export const NavBar = () => {
-
+export default function NavBar() {
   return (
     <>
-      
+      <nav className="grid grid-cols-3 items-center bg-slate-800 p-4 text-white">
+      {/* Logo Section */}
+      <div className="flex justify-start">
+        <img src={logo} alt="logo" className="w-16 object-contain" />
+      </div>
+
+      {/* Center Links (02) */}
+      <div className="text-center">Menu Items</div>
+
+      {/* Right Section (03) */}
+      <div className="text-right">Profile / Sign In</div>
+    </nav>
     </>
-  )
+  );
 }
